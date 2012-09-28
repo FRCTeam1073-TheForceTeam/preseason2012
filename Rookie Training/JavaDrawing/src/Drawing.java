c8682365df1a09d045e414721f3fd2e3ec77e4b3
Evin Ugur <evinoog96@gmail.com>
2012-09-28 08:51:47 -0400

    Rookie Drawing Project
    Added in the Triangle Code from last night and some more advanced
    drawing files if someone wants to play with them.

---
+++ Rookie Training/JavaDrawing/src/Drawing.java
@@ -0,0 +1,9 @@
+import java.awt.Component;
+import java.awt.Graphics;
+import java.awt.Color;
+public class Drawing extends Component{
+	public Drawing(){}
+	public void paint(Graphics g){
+		//empty drawing
+	}
+}
---
+++ Rookie Training/JavaDrawing/src/SmartTriangle.java
@@ -0,0 +1,24 @@
+import java.awt.Component;
+import java.awt.Graphics;
+import java.awt.Color;
+public class SmartTriangle extends Component{
+	private Color triangleColor;
+	public SmartTriangle(Color triangleColor){
+		this.triangleColor = triangleColor;
+	}
+	public void paint(Graphics g){
+		g.setColor(triangleColor);
+		g.drawLine(x1(), y1(), x2(), y2());
+		g.drawLine(x1(), y1(), x3(), y3());
+		g.drawLine(x2(), y2(), x3(), y3());
+		
+	}
+	protected int x1(){return 40;}
+	protected int y1(){return 100;}
+	
+	protected int x2(){return 300;}
+	protected int y2(){return 200;}
+	
+	protected int x3(){return 223;}
+	protected int y3(){return 42;}
+}
