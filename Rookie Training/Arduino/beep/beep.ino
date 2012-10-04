a3571c897753336fb6a3f6cb3d08fed0d2b09065
Kate <katecherian14159@gmail.com>
2012-10-03 21:44:31 -0400

    Expanded on beep to include stuff with LEDs
    Rookies - Matt Thyng, Peter Peloquin, Patrick Clark, Gintas Paradie

--- Rookie Training/Arduino/beep/beep.ino
+++ Rookie Training/Arduino/beep/beep.ino
@@ -1,30 +1,54 @@
 #include "pitches.h"
-
-int led = 3;
+#define SHORT_DELAY 100
+int led1 = 2;
+int led2 = 3;
+int led3 = 4;
+int led4 = 5;
+int led5 = 6;
 
 int speaker = 8;
 
 int note = 31;
+int lowestpitch = 20;
+int highestpitch = 6000;
 
 int finished = 0;
 
+int delayT = 100;
+
 void setup() {
-  pinMode(led, OUTPUT);
+  pinMode(led1, OUTPUT);
+  pinMode(led2, OUTPUT);
+  pinMode(led3, OUTPUT);
+  pinMode(led4, OUTPUT);
+  pinMode(led5, OUTPUT);
   pinMode(speaker, OUTPUT);
 }
 
 void loop() {
-    tone(speaker, note, 500);
-    tone(led, note/100, 2000);
+    tone(speaker, note, 2000);
+    digitalWrite(led1, HIGH);
+    delay(delayT);
+    digitalWrite(led1, LOW);
+    digitalWrite(led2, HIGH);
+    digitalWrite(led2, LOW);
+    digitalWrite(led3, HIGH);
+    digitalWrite(led3, LOW);
+    digitalWrite(led4, HIGH);
+    digitalWrite(led4, LOW);
+    digitalWrite(led5, HIGH);
+    digitalWrite(led5, LOW);
+    //tone(speaker, 0, 2000);
+    //tone(led, note/100, 2000);
    // digitalWrite(led, HIGH);
     
-    if (note<4978 && !finished){
+    if (note<highestpitch && !finished){
       note = note+1;
     }
-    if(note>31 && finished){
+    if(note>lowestpitch && finished){
       note = note-1;  
     }
-    if(note == 4978){
+    if(note >= highestpitch){
       finished=1;
       //digitalWrite(led, HIGH);
     }
